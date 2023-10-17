026a320a22f1592a253943155348ef58350ee38e
Palatip Jantawong <palatipjant@outlook.com>
2023-10-17 07:45:33 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Palatip Jantawong on 17/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
